d255dc71bf1b45e5dc9dfc7c4e036f1c6ec1ce23
Stefan Kennedy <stefan.kennedy48@mail.dcu.ie>
2016-10-05 16:49:48 +0100

    Added method for clients to provide a unique identifier

--- dipla/server/server.py
+++ dipla/server/server.py
@@ -1,13 +1,18 @@
 import asyncio
 import websockets
 
-connected = set()
+connected = {}
 
 async def handler(websocket, path):
-    connected.add(websocket)
-    await websocket.send(str(connected))
+    if(path[1:] in connected.keys()):
+        await websocket.send("Sorry, this Agent ID is taken")
+        return
+    connected[path[1:]]=(websocket)
+    await websocket.send("Online users: " + ", ".join(connected.keys()))
+    
+    
 
-start_server = websockets.serve(handler, 'localhost', 8765)
+start_server = websockets.serve(handler, "localhost", 8765)
 
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
