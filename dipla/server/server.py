49394f2c86ee38d4f2d73bf7e5fd961161b4c94f
Stefan Kennedy <stefan.kennedy48@mail.dcu.ie>
2016-10-05 18:11:32 +0100

    Refactored to comply with style guide, include dependency in virtualenv and added comment

--- dipla/server/server.py
+++ dipla/server/server.py
@@ -4,13 +4,23 @@ import websockets
 connected = {}
 
 async def handler(websocket, path):
-    if(path[1:] in connected.keys()):
+    user_id = path[1:]
+
+    if(user_id in connected.keys()):
         await websocket.send("Sorry, this Agent ID is taken")
         return
-    connected[path[1:]]=(websocket)
-    await websocket.send("Online users: " + ", ".join(connected.keys()))
-    
-    
+
+    connected[user_id] = (websocket)
+    try:
+        await websocket.send("Online users: " + ", ".join(connected.keys()))
+        while True:
+            # recv() raises a ConnectionClosed exception when the client
+            # disconnects, which breaks out of the while True loop.
+            message = await websocket.recv()
+            print(message)
+    finally:
+        del connected[user_id]
+
 
 start_server = websockets.serve(handler, "localhost", 8765)
 
