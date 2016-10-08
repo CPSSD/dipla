e59e02b2332fb7d498b0c203096cc57208fa8cf8
Stefan Kennedy <stefan.kennedy48@mail.dcu.ie>
2016-10-08 22:10:39 +0100

    Added test for popping items from task_queue

--- tests/server/task_queue_test.py
+++ tests/server/task_queue_test.py
@@ -10,4 +10,11 @@ class TaskQueueTest(unittest.TestCase):
         self.assertEqual(popped.data_instructions, "data instructions")
         self.assertEqual(popped.task_instructions, "task instructions")
 
-
+    def test_pop_tasks(self):
+        queue = task_queue.TaskQueue()
+        queue.add_task("1", "")
+        queue.add_task("2", "")
+        self.assertEqual(queue.pop_task().data_instructions, "1")
+        self.assertEqual(queue.pop_task().data_instructions, "2")
+        queue.add_task("3", "")
+        self.assertEqual(queue.pop_task().data_instructions, "3")
