25912e3daa23070c9ecad93129ea191b91a0b7be
byxor <brandnewbyxor@gmail.com>
2016-10-12 22:15:45 +0000

    Add unfinished code to make commits easier

--- dipla/client/binary.py
+++ dipla/client/binary.py
@@ -1,13 +1,32 @@
+from subprocess import Popen, PIPE
 import os
 
 
 class BinaryRunner(object):
 
     def __init__(self):
-        pass
+        self._process = None
+        self._running = False
 
-    def run(self, filepath):
-        print(filepath)
+    def run(self, command):
+        arguments = command.split(" ")
+        filepath = arguments[0]
         if not os.path.exists(filepath):
             raise FileNotFoundError
+        else:
+            self._process = Popen(arguments, shell=False, stdin=PIPE, stdout=PIPE)
+            self._running = True
 
+    def send_stdin(self, message):
+        self._process.stdin.write(message.encode("utf-8"))
+        self._process.stdin.flush()
+
+    def is_running(self):
+        
+#        p = self._process
+#        print(p.stdout.read())
+#        print(p.poll())
+
+
+        self.running = self._process.poll() == None
+        return self._running
--- tests/client/binary_test.py
+++ tests/client/binary_test.py
@@ -1,4 +1,5 @@
 import unittest
+import time
 import os
 from dipla.client.binary import BinaryRunner
 from dipla.environment import PROJECT_DIRECTORY
@@ -6,20 +7,49 @@ from dipla.environment import PROJECT_DIRECTORY
 
 class BinaryRunnerTest(unittest.TestCase):
 
+    EXAMPLE_BINARY_PATH = PROJECT_DIRECTORY + "tests/example_binary/builds/ARM"
+
     def test_that_error_is_raised_when_running_non_existent_binary(self):
         self.given_the_binary("~/fake_binary")
         self.then_running_the_binary_will_raise_a_FileNotFoundError()
 
     def test_that_no_error_is_raised_when_running_existing_binary(self):
-        self.given_the_binary(PROJECT_DIRECTORY + "tests/example_binary/builds/example_binary_ARM")
+        self.given_using_the_example_binary()
         self.then_running_the_binary_will_not_raise_any_exceptions()
+
+    def test_that_binary_is_running_after_no_commands_have_been_given(self):
+        self.given_using_the_example_binary()
+        self.when_the_binary_is_run()
+        self.then_the_binary_will_be_running()
+
+#    def test_that_binary_is_running_after_an_arbitrary_command_has_been_given(self):
+#        self.given_using_the_example_binary()
+#        self.when_the_binary_is_run()
+#        self.and_the_standard_input_is_injected("arbitrary input")
+#        self.then_the_binary_will_be_running()
+#
+#    def test_that_binary_stops_running_after_the_exit_command_is_given(self):
+#        self.given_using_the_example_binary()
+#        self.when_the_binary_is_run()
+#        self.and_the_standard_input_is_injected("!")
+#        self.and_we_wait_for_a_few_seconds()
+#        self.then_the_binary_will_not_be_running()
 
     def given_the_binary(self, filepath):
         self.filepath = filepath
 
+    def given_using_the_example_binary(self):
+        self.given_the_binary(BinaryRunnerTest.EXAMPLE_BINARY_PATH + ' foo')
+
     def when_the_binary_is_run(self):
         self.binary_runner = BinaryRunner()
         self.binary_runner.run(self.filepath)
+
+    def and_we_wait_for_a_few_seconds(self):
+        time.sleep(3)
+
+    def and_the_standard_input_is_injected(self, command):
+        self.binary_runner.send_stdin(command)
 
     def then_running_the_binary_will_raise_a_FileNotFoundError(self):
         self.assertRaises(FileNotFoundError, self.when_the_binary_is_run)
@@ -28,5 +58,11 @@ class BinaryRunnerTest(unittest.TestCase):
         try:
             self.when_the_binary_is_run()
         except:
-            self.fail("Running the binary raised an exception when it shouldn't have")
+            self.fail("Running the binary raised an exception" +
+                      "when it shouldn't have")
 
+    def then_the_binary_will_not_be_running(self):
+        self.assertFalse(self.binary_runner.is_running())
+
+    def then_the_binary_will_be_running(self):
+        self.assertTrue(self.binary_runner.is_running())
