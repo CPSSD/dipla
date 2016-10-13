2f8201e207253086bd20096ae6945f43d5e37811
byxor <brandnewbyxor@gmail.com>
2016-10-13 22:45:55 +0000

    Fix StreamReader to be stoppable

--- dipla/shared/stream_reader.py
+++ dipla/shared/stream_reader.py
@@ -1,48 +1,40 @@
 import time
 from queue import Queue
 from threading import Thread
+from threading import Event
+from nonblock import nonblock_read
 from .logutils import get_logger
 from .continuous_stream_poller import ContinuousStreamPoller
 
 
 class StreamReader(object):
 
-    def __init__(self, continuous_stream_poller):
+    def __init__(self, stream):
         self._logger = get_logger(__name__)
-        self._stream_poller = continuous_stream_poller
-        self._stream_queue = continuous_stream_poller._queue
+        self._stream = stream 
+        self._queue = Queue()
+        self._stream_poller = ContinuousStreamPoller(self._stream, self._queue)
         self._reading = False
+        self._stop_request = Event()
 
     def open(self):
-        self._logger.debug("StreamReader: about to open.")
-        self._running = True
+        self._logger.debug("StreamReader: about to open...")
         self._stream_poller.start()
-        self._logger.debug("StreamReader: opened.")
-
+       
     def read_line_without_waiting(self):
-        self._logger.debug("StreamReader: about to read without waiting.")
         self._reading = True
-        pass
         self._reading = False
-        self._logger.debug("StreamReader: finished read without waiting.")
 
     def read_line_with_waiting(self):
-        self._logger.debug("StreamReader: about to read with waiting.")
         self._reading = True
-        while self._running:
-            if not self._stream_queue.empty():
-                break
-            self._logger.debug("StreamReader: stream queue is empty." +
-                               "Sleeping for 0.1s before retrying...")
-            time.sleep(0.1)
-        self._reading = False
-        self._logger.debug("StreamReader: finished read with waiting.")
+        start_time = time.time()
+        while not self._stop_request.isSet():
+            if not self._queue.empty():
+                self._reading = False
+                return
+
 
     def close(self):
         self._logger.debug("StreamReader: about to close...")
-        self._stream_poller.stop()
-        self._running = False
-        self._logger.debug("StreamReader: closed.")
-
-    def currently_reading(self):
-        return self._reading
+        self._stop_request.set()
+        self._stream_poller.join()
