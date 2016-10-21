c3dda1f6f3c93fc4b234f5b58383b8e9d88e301b
byxor <brandnewbyxor@gmail.com>
2016-10-22 00:47:41 +0100

    Remove code to auto-initialise logger

--- dipla/shared/logutils.py
+++ dipla/shared/logutils.py
@@ -2,24 +2,19 @@ from logging import *
 import threading
 
 
-LOGGER_INITIALISED = False
-
-
-def get_logger(module_name, level=DEBUG, handler=FileHandler("DIPLA.log"),
-               format_="%(asctime)s - %(levelname)s - %(name)s - %(message)s"):
-
-    logger = getLogger(module_name)
-
-    global LOGGER_INITIALISED
-    if LOGGER_INITIALISED:
-        return logger
-
+# Initialises the logger.
+#
+# Allows custom logging configuration to be set.
+#
+# WARNING:
+# Calling this multiple times will result in undesired behaviour
+# such as duplicated log output.
+#
+def init(level=DEBUG,
+         handler=FileHandler("DIPLA.log"),
+         format_="%(asctime)s - %(levelname)s - %(name)s - %(message)s)"):
     logger.setLevel(level)
-
     formatter = Formatter(format_)
     handler.setFormatter(formatter)
-
     logger.addHandler(handler)
-
-    LOGGER_INITIALISED = True
     return logger
