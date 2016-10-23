7fa6d29980d89add8c5e20f7d3f84596ae20fd73
byxor <brandnewbyxor@gmail.com>
2016-10-23 23:25:47 +0100

    Initialise services in run_client.py

--- run_client.py
+++ run_client.py
@@ -1,12 +1,33 @@
 from dipla.client.client import Client
+from dipla.client.client_services import BinaryRunnerService
 from dipla.shared import logutils
 import sys
 from logging import FileHandler
 
-if __name__ == '__main__':
+
+def main(argv):
+    init_logger(argv)
+    services = create_services()
+    client = Client('ws://localhost:8765', services)
+    client.start()
+
+
+def init_logger():
     loc = 'DIPLA.log'
-    if len(sys.argv) == 2:
-        loc = sys.argv[1]
+    if len(argv) == 2:
+        loc = argv[1]
     logutils.init(handler=FileHandler(loc))
-    c = Client('ws://localhost:8765')
-    c.start()
+
+
+def create_services():
+    services = {"run_binary": _create_binary_runner_service()}
+    return services
+
+
+def _create_binary_runner_service():
+    binary_runner = CommandLineBinaryRunner()
+    return BinaryRunnerService(binary_runner)
+
+
+if __name__ == '__main__':
+    main(sys.argv)
