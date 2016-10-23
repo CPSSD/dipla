2d75c1abe586490bf377f86f52eb173579c1a1cf
iandioch <iandioch11@gmail.com>
2016-10-23 17:35:05 -0400

    Add handler to call to init

--- run_client.py
+++ run_client.py
@@ -4,9 +4,9 @@ import sys
 from logging import FileHandler
 
 if __name__ == '__main__':
-    logutils.init()
     loc = 'DIPLA.log'
     if len(sys.argv) == 2:
         loc = sys.argv[1]
+    logutils.init(handler=FileHandler(loc))
     c = Client('ws://localhost:8765')
     c.start()
