1918e1aef521883c8b79670b61a816a08688ab51
Stefan Kennedy <stefan.kennedy48@mail.dcu.ie>
2016-11-11 13:06:03 +0000

    Added class to prevent replicating UID generation code

---
+++ dipla/shared/uid_generator.py
@@ -0,0 +1,22 @@
+
+def generate_uids(existing_uids, length, safe = True,
+                  choices = "abcdefghijklmnopqrstuvwxyz0123456789"):
+    # Remove duplicates from the choices string
+    choices = ''.join(set(choices))
+
+    # If safe mode is activated, raise an error if it is possible
+    # that this search for a uid could run infintitely because
+    # there are no unique IDs left to generate
+    if safe and len(existing) == len(choices)**length:
+        raise IDsExhausted("""Safe mode active and it is
+            possible that all UIDs have been exhausted""")
+
+
+    while True:
+        suggested_uid = ''.join(
+                random.choice(choices) for i in range(length))
+        if not suggested_uid in existing_uids:
+            return suggested_uid
+
+class IDsExhausted(Exception):
+    pass
