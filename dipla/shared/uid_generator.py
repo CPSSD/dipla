2b2f23fb169bae5fa4ea5d2faad14248359b8c1f
StefanKennedy <stefan.kennedy48@mail.dcu.ie>
2016-11-13 19:52:08 +0000

    Added documentation to uid_generator module

--- dipla/shared/uid_generator.py
+++ dipla/shared/uid_generator.py
@@ -1,7 +1,15 @@
+""" UID Generator
+
+This module contains functionality for producing unique IDs, doing checks
+on a set of existing IDs and allowing the user to be sure that they can
+generate a unique ID with their choices in a quick runtime
+"""
+
 import random
 
-def generate_uid(existing_uids, length, safe = True,
-                 choices = "abcdefghijklmnopqrstuvwxyz0123456789"):
+
+def generate_uid(existing_uids, length, safe=True,
+                 choices="abcdefghijklmnopqrstuvwxyz0123456789"):
     # Remove duplicates from the choices string
     choices = ''.join(set(choices))
 
@@ -12,12 +20,12 @@ def generate_uid(existing_uids, length, safe = True,
         raise IDsExhausted("""Safe mode active and it is
             possible that all UIDs have been exhausted""")
 
-
     while True:
         suggested_uid = ''.join(
                 random.choice(choices) for i in range(length))
-        if not suggested_uid in existing_uids:
+        if suggested_uid not in existing_uids:
             return suggested_uid
+
 
 class IDsExhausted(Exception):
     pass
