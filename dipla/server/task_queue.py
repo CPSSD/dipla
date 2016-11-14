71a0eb193587d84532bb14ca8884268f7a86261e
CianLR <cian.ruane1@gmail.com>
2016-11-14 12:47:51 +0000

    Fix style to conform to PEP8

--- dipla/server/task_queue.py
+++ dipla/server/task_queue.py
@@ -38,7 +38,7 @@ class TaskQueue:
             new_node = TaskQueueNode(
                 item, container_queue=self, previous_node=self.queue_tail)
             self.queue_tail.next_node = new_node
-            self.queue_tail = new_node 
+            self.queue_tail = new_node
 
     def pop_task(self):
         """
