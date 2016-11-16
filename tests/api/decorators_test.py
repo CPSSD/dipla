94c9c885ca4e5a46b844cef8699e2a8b5a3c7ed0
byxor <brandnewbyxor@gmail.com>
2016-11-16 19:03:29 +0000

    Add task_input decorator and tests

--- tests/api/decorators_test.py
+++ tests/api/decorators_test.py
@@ -1,5 +1,6 @@
 from unittest import TestCase
 from dipla.api.decorators import distributable
+from dipla.api.decorators import task_input
 
 
 class DistributableDecoratorTest(TestCase):
@@ -31,6 +32,40 @@ class DistributableDecoratorTest(TestCase):
 
     def _apply_distributable_decorator(self):
         @distributable
+        def foo():
+            pass
+        return foo
+
+
+class TaskInputDecoratorTest(TestCase):
+
+    def test_that_task_input_decorator_can_be_imported(self):
+        self.given_a_task_input_decorator()
+        self.when_it_is_imported()
+        self.then_no_errors_are_thrown()
+
+    def test_that_task_input_decorator_can_be_applied(self):
+        self.given_a_task_input_decorator()
+        self.when_it_is_applied_to_a_function()
+        self.then_no_errors_are_thrown()
+
+    def given_a_task_input_decorator(self):
+        pass
+
+    def when_it_is_imported(self):
+        self.operation = self._import_task_input_decorator
+
+    def when_it_is_applied_to_a_function(self):
+        self.operation = self._apply_task_input_decorator
+
+    def then_no_errors_are_thrown(self):
+        self.operation()
+
+    def _import_task_input_decorator(self):
+        from dipla.api.decorators import task_input
+
+    def _apply_task_input_decorator(self):
+        @task_input
         def foo():
             pass
         return foo
