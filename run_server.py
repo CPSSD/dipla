d174c1f266242a68f1e40b830711145710b352fb
StefanKennedy <stefan.kennedy48@mail.dcu.ie>
2016-11-24 18:38:53 +0000

    fixup! Remove first iteration tracking of TaskQueue launching new tasks when ready

--- run_server.py
+++ run_server.py
@@ -4,20 +4,6 @@ from dipla.server.task_queue import TaskQueue, Task
 
 def main():
     tq = TaskQueue()
-    tasks = [
-        Task.create('add', {'x': 'int', 'y': 'int'}),
-        Task.create('add', {'x': 'int', 'y': 'int'}),
-        Task.create('add', {'x': 'int', 'y': 'int'})
-    ]
-    for task in tasks:
-        tq.push_task(task)
-        print('Added task %s' % task.task_instructions)
-    tq.add_new_data('int', 1)
-    tq.add_new_data('int', 2)
-    tq.add_new_data('int', 32)
-    tq.add_new_data('int', 4)
-    tq.add_new_data('int', 16)
-    tq.add_new_data('int', 4)
 
     bm = BinaryManager()
     bm.add_platform('.*ix.*', [
