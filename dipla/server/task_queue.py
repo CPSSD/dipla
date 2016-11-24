c847c46a0f96b4aa21fc8583aa0ee5c6f049d26a
StefanKennedy <stefan.kennedy48@mail.dcu.ie>
2016-11-24 18:27:31 +0000

    Remove first iteration tracking of TaskQueue launching new tasks when ready

--- dipla/server/task_queue.py
+++ dipla/server/task_queue.py
@@ -20,11 +20,6 @@ class TaskQueue:
         self.queue_head = None
         self.queue_tail = None
 
-        # A list of (data_type, value) tuples
-        self.ready_data = []
-        # A list of Tasks that do not have all the data they need yet
-        self.waiting_tasks = []
-
     def push_task(self, item):
         """
         Adds a task to the queue, or to the pool of waiting tasks if it
@@ -54,37 +49,6 @@ class TaskQueue:
                 item, container_queue=self, previous_node=self.queue_tail)
             self.queue_tail.next_node = new_node
             self.queue_tail = new_node
-
-    def update_waiting_tasks(self):
-        """Taking the newly received data, see if any tasks are now ready
-        to go."""
-        # Iterate backwards so we can delete things without messing up
-        # indexes
-        for i in range(len(self.ready_data) - 1, -1, -1):
-            data_type, value = self.ready_data[i]
-            found = False
-            # Find a task that needs this data, and give it to it
-            for j in range(len(self.waiting_tasks)):
-                if self.waiting_tasks[j].requires_type(data_type):
-                    found = True
-                    self.waiting_tasks[j].give_data(data_type, value)
-                    break
-            if found:
-                del self.ready_data[i]
-
-        # Check for tasks that are ready to go
-        for i in range(len(self.waiting_tasks) - 1, -1, -1):
-            if self.waiting_tasks[i].ready():
-                # Add this task to the live queue
-                self.push_task(self.waiting_tasks[i])
-                # Delete this task from the waiting list
-                del self.waiting_tasks[i]
-
-    def add_new_data(self, data_type, value):
-        """Add some new data to the pool and see if it makes any tasks
-        ready to go."""
-        self.ready_data.append((data_type, value))
-        self.update_waiting_tasks()
 
     def pop_task(self):
         """
@@ -197,32 +161,6 @@ class Task:
         self.completion_check = completion_check
         self.completed = False
 
-    @staticmethod
-    def create(task_instructions,
-               expected_data,
-               completion_check=lambda x: True):
-        """Create a Task.
-
-        Params:
-         - task_instructions: See __init__
-         - expected_data: A dict in the form of {name:type}, where both
-        "name" and "type" are strings. "name" refers to the name of a
-        DataInstruction that this Task will need as input, and "type"
-        refers to the type of that DataInstruction.
-         - completion_check: See __init__
-
-        Example usage:
-        Task.create('add', {'a': 'number', 'b': 'number })
-
-        Returns:
-        A Task.
-        """
-        data_instructions = [DataInstruction(k, expected_data[k])
-                             for k in expected_data.keys()]
-        return Task(data_instructions,
-                    task_instructions,
-                    completion_check)
-
     def add_result(self, result):
         if self.completion_check(result):
             self._complete_task()
@@ -232,58 +170,3 @@ class Task:
             # Take this element out of the LinkedList
             self.container_node.pop()
         self.completed = True
-
-    def ready(self):
-        """Returns True if the task has all the data it needs to run"""
-        return all([x.ready() for x in self.data_instructions])
-
-    def requires_type(self, data_type):
-        return [x for x in self.data_instructions if (
-            not x.ready() and x.get_type() == data_type)]
-
-    def give_data(self, data_type, value):
-        for i in range(len(self.data_instructions)):
-            if ((not self.data_instructions[i].ready()) and
-                    self.data_instructions[i].get_type() == data_type):
-                self.data_instructions[i].set_value(value)
-                return
-
-
-class DataInstruction:
-    """
-    This is a class to hold a data input to a client binary.
-    It can contain either immediate data ready to be used, or a
-    piece of future data being waited on before being ready to be used.
-    """
-
-    def __init__(self, name, data_type="immediate", value=None):
-        """Initialises the data instruction.
-
-        Params:
-         - name: The key that will be used for this piece of data when
-        it is being passed to the binary.
-         - data_type: A string with the type of data. This will be used
-        to figure out if newly available data is relevant to a particular
-        Task.
-         - value: The actual value of this data instruction, of any type.
-        """
-        self.name = name
-        # private, with a get_(), as some data may be drawn from a stream,
-        # database, or similar.
-        self._data_type = data_type
-        self._value = value
-
-    def ready(self):
-        """Returns True if this DataInstruction is ready to be sent, and
-        False if it is still waiting to be fulfilled."""
-        return self._value is not None
-
-    def get_value(self):
-        """Returns the underlying data of this container."""
-        return self._value
-
-    def set_value(self, value):
-        self._value = value
-
-    def get_type(self):
-        return self._data_type
