769ca14771aba6d32c3c86504c9d0ac232477943
StefanKennedy <stefan.kennedy48@mail.dcu.ie>
2016-11-28 14:31:03 +0000

    Change run_server to supply tasks with a generated id

--- run_server.py
+++ run_server.py
@@ -1,14 +1,18 @@
 from dipla.server.server import Server, BinaryManager
 from dipla.server.task_queue import TaskQueue, Task
+from dipla.shared import uid_generator
 
 def distributable_squarer(value):
     return value**2
 
 def main():
     tq = TaskQueue()
-
+    
+    task_uid = uid_generator.generate_uid(
+        length = 8, existing_uids=[])
     source = [1, 2, 3, 4, 5]
-    tq.push_task(Task(source, 'fibonacci'))
+
+    tq.push_task(Task(task_uid, source, 'fibonacci'))
 
     bm = BinaryManager()
     bm.add_platform('.*x.*', [
