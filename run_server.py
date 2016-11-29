ff8e0ec6b27447a252c47e872a0b7c3d6342f1f0
StefanKennedy <stefan.kennedy48@mail.dcu.ie>
2016-11-29 23:40:53 +0000

    Update run_server.py to use the new way of creating tasks

--- run_server.py
+++ run_server.py
@@ -1,5 +1,5 @@
 from dipla.server.server import Server, BinaryManager
-from dipla.server.task_queue import TaskQueue, Task
+from dipla.server.task_queue import TaskQueue, Task, DataSource
 from dipla.shared import uid_generator
 
 def distributable_squarer(value):
@@ -8,16 +8,25 @@ def distributable_squarer(value):
 def main():
     tq = TaskQueue()
     
+    root_source = [1, 2, 3, 4, 5]
+
+    # Create a first task that depends on root_source, a iterable
+    # collection
     first_task_uid = uid_generator.generate_uid(
         length = 8, existing_uids=[])
+    first_task = Task(first_task_uid, 'fibonacci')
+    first_task.add_data_source(
+        DataSource.create_source_from_iterable(root_source))
+
+    # Create a second task that depends on the first task's output
     second_task_uid = uid_generator.generate_uid(
         length = 8, existing_uids=[first_task_uid])
-    source = [1, 2, 3, 4, 5]
+    second_task = Task(second_task_uid, 'fibonacci')
+    second_task.add_data_source(
+        DataSource.create_source_from_task(first_task))
 
-    tq.push_task(Task(first_task_uid, source, 'fibonacci'))
-    tq.push_task(
-        Task(second_task_uid, source, 'fibonacci'),
-        dependencies=[first_task_uid])
+    tq.push_task(first_task)
+    tq.push_task(second_task)
 
     bm = BinaryManager()
     bm.add_platform('.*ix.*', [
