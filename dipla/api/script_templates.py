ff535e601045d667b940c94c5beae95a9e86f0aa
CianLR <cian.ruane1@gmail.com>
2016-11-30 20:11:48 +0000

    Add function serialisation

---
+++ dipla/api/script_templates.py
@@ -0,0 +1,22 @@
+# This file is intended to store the templates for the scripts the clients will run.
+# The '{}' will be replaced with the base64 of the pickle of the deconstructed function
+# code object.
+
+argv_input_script = """#! /usr/bin/python3
+from base64 import b64decode
+from types import CodeType
+import pickle
+import sys
+
+encoded_code = {}
+pickled_code = b64decode(encoded_code)
+codeobject_data = pickle.loads(pickled_code)
+func_code = CodeType(*codeobject_data)
+
+def unwraped_func():
+    pass
+
+unwraped_func.__code__ = func_code
+
+print(unwraped_func(*sys.argv[1:]))
+"""
