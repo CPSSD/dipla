cfb4ee20021c9ce1f0b5164fc4ba15dde8c7ab15
StefanKennedy <stefan.kennedy48@mail.dcu.ie>
2016-12-02 20:57:30 +0000

    fixup! Resolved incorrect creation of argument_values in integration test

--- tests/example_binaries/web_count/web_count.cpp
+++ tests/example_binaries/web_count/web_count.cpp
@@ -6,9 +6,48 @@
  */
 
 #include <iostream>
+#include <curl/curl.h>
+
+using namespace std;
+
+size_t write_callback(char* contents, size_t size,
+                      size_t nmemb, string *databuff) {
+  databuff->append(contents, size * nmemb);
+  return size * nmemb;
+}
+
+int count_substr(const string& base, const string& sub) {
+  int count = 0;
+  size_t pos = base.find(sub, 0);
+  while(pos != string::npos) {
+    ++count;
+    pos = base.find(sub, pos + 1);
+  }
+  return count;
+}
 
 int main(int argc, char* argv[]) {
-  using namespace std;
-  cout << "test" << endl;
+  
+  if (argc < 3) {
+    cout << "Usage: web_count <url> <search_term>" << endl;
+    return 1;
+  }
+
+  CURL *curl = curl_easy_init();
+  CURLcode res;
+  string response;
+  curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+  curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
+  curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response);  
+
+  curl_easy_setopt(curl, CURLOPT_URL, argv[1]);
+  res = curl_easy_perform(curl);
+  if(res != CURLE_OK) {
+    cout << "-1";
+  } else {
+    cout << count_substr(response, argv[2]) << endl;
+  }
+  
+  curl_easy_cleanup(curl);
   return 0;
 }
