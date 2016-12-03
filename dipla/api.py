efbc79e1f921c12d1dfe6c3cb3329ff86ea0b1a0
CianLR <cian.ruane1@gmail.com>
2016-12-03 20:33:48 +0000

    Add example 'apply_distributable' and PEP8

--- dipla/api.py
+++ dipla/api.py
@@ -1,6 +1,7 @@
 from dipla.api_support.function_serialise import get_encoded_script
 from dipla.server.server import Server, BinaryManager
 
+
 class Dipla:
 
     # BinaryManager to be injected into server.
@@ -11,14 +12,27 @@ class Dipla:
 
     @staticmethod
     def distribute(function):
+        """
+        Takes a function and converts it to a binary, the binary is then
+        registered with the BinaryManager. The function is returned unchanged.
+        """
+        # Turn the function into a base64'd Python script.
         base64_binary = get_encoded_script(function)
+        # Register the result as a new binary for any platform with the name
+        # of the function as the task name. 
         Dipla.binary_manager.add_encoded_binaries('.*', [
             (function.__name__, base64_binary),
         ])
+        # Don't actually modify the final function.
+        return function
 
     @staticmethod
     def data_source(function):
         pass
+
+    @staticmethod
+    def apply_distributable(function, stream):
+        task_name = function.__name__
 
     @staticmethod
     def start():
