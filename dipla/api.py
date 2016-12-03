1e1708e0586a16b96ffd1ace6b5615d6494504d5
CianLR <cian.ruane1@gmail.com>
2016-12-03 19:43:46 +0000

    Add 'distribute' decorator code

--- dipla/api.py
+++ dipla/api.py
@@ -1,14 +1,20 @@
 from dipla.api_support.function_serialise import get_encoded_script
-from dipla.server.server import BinaryManager
+from dipla.server.server import Server, BinaryManager
 
 class Dipla:
 
-    binary_mananger = BinaryManager()
+    # BinaryManager to be injected into server.
+    binary_manager = BinaryManager()
+
+    # Server instance.
+    server = None
 
     @staticmethod
     def distribute(function):
         base64_binary = get_encoded_script(function)
-        #Dipla.binary_mananger.add_platform('.*', )
+        Dipla.binary_manager.add_encoded_binaries('.*', [
+            (function.__name__, base64_binary),
+        ])
 
     @staticmethod
     def data_source(function):
@@ -17,4 +23,12 @@ class Dipla:
     @staticmethod
     def start():
         # Start the dipla server
+
+        # This comment is included to illustrate how I expect the start() to
+        # look, the final implementation may vary.
+        # Dipla.server = Server(
+        #     Dipla.tq,
+        #     Dipla.binary_manager
+        # )
+        # Dipla.server.start()
         pass
