d3722e0e1a4b13db4298b40a8bd21a747420b401
CianLR <cian.ruane1@gmail.com>
2016-12-03 20:53:22 +0000

    Add unit tests for function serialisation

---
+++ tests/api/function_serialise_test.py
@@ -0,0 +1,44 @@
+from unittest import TestCase
+from base64 import b64decode
+from pickle import loads
+from types import CodeType
+from dipla.api_support.function_serialise import serialise_code_object
+from dipla.api_support.function_serialise import get_encoded_script
+
+
+class SerialiseCodeObjectTest(TestCase):
+
+    def test_code_object_can_be_pickled(self):
+        example_func = self._get_example_function()
+        pickled_co = serialise_code_object(example_func.__code__)
+
+    def test_pickled_code_object_is_valid(self):
+        example_func = self._get_example_function()
+        pickled_co = serialise_code_object(example_func.__code__)
+        decoded_co = loads(pickled_co)
+
+        co = CodeType(*decoded_co)
+        self.assertEqual(co, example_func.__code__)
+
+    def _get_example_function(self):
+        def example_func(x, y):
+            return x + y
+        return example_func
+
+
+class ScriptEncoderTest(TestCase):
+
+    def test_function_can_be_encoded(self):
+        example_func = self._get_example_function()
+        b64_script = get_encoded_script(example_func)
+
+    def test_encoded_script_valid_base64(self):
+        example_func = self._get_example_function()
+        b64_script = get_encoded_script(example_func)
+        decoded_script = b64decode(b64_script)
+        self.assertEqual(type(decoded_script), bytes)
+
+    def _get_example_function(self):
+        def example_func(x, y):
+            return x + y
+        return example_func
