8a5b0017356f0efb93eab036927869e32ef932fd
StefanKennedy <stefan.kennedy48@mail.dcu.ie>
2016-12-04 15:36:24 +0000

    Updated hello world example to use the new api

--- examples/hello_world.py
+++ examples/hello_world.py
@@ -4,8 +4,10 @@ sys.path.append(path.abspath('../dipla'))
 
 from dipla.api import Dipla
 
-input_data = [1, 2, 3, 4, 5]
+input_data = ["1", "2", "3", "4", "5"]
+def read_function(input_value):
+    return int(input_value) * 2
 
 # Convert the input_data list to a form that dipla can use for
 # distributing
-data_source = Dipla.read_data_source(input_data) 
+data_source = Dipla.read_data_source(read_function, input_data) 
