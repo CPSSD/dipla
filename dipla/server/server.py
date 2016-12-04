399491a3ceecbe968f845e20b26a95319c886130
StefanKennedy <stefan.kennedy48@mail.dcu.ie>
2016-12-04 15:41:24 +0000

    Improved comment explaining task distribution when a task receives its result

--- dipla/server/server.py
+++ dipla/server/server.py
@@ -119,6 +119,9 @@ class ServerServices:
             return None
         # If there was extra tasks that no others could do, try and
         # assign it to this worker, as it should be the only ready one
+        # assign it to this worker, as it should be the only ready one.
+        # If there are other workers it is okay to distribute tasks to
+        # them too
         params.server.distribute_tasks()
         return None
 
