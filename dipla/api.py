24c36fd490dc70a3417a4dc7c01479742dab9e51
iandioch <iandioch11@gmail.com>
2016-12-06 12:06:54 -0500

    Add skeleton of apply_distributable

--- dipla/api.py
+++ dipla/api.py
@@ -1,11 +1,13 @@
 from dipla.api_support.function_serialise import get_encoded_script
 from dipla.server.server import BinaryManager
+from dipla.server.task_queue import TaskQueue, Task
 
 
 class Dipla:
 
     # BinaryManager to be injected into server.
     binary_manager = BinaryManager()
+    task_queue = TaskQueue()
 
     @staticmethod
     def distributable(function):
@@ -26,6 +28,16 @@ class Dipla:
     @staticmethod
     def data_source(function):
         return function
+
+    @staticmethod
+    def apply_distributable(function, *args):
+        uid = None
+        task = Task(uid, function.__name__)
+	for arg in args:
+            pass
+        task_queue.push_task(task)
+        return task
+        
 
 # Remember that the function's __name__ is the task name in apply_distributable
 # task_name = function.__name__
