42d9de47f4fdee6ff4891a1ba5a34fb9fc91e111
CianLR <cian.ruane1@gmail.com>
2016-12-06 15:04:35 +0000

    Add basic QualityTest class

---
+++ dipla/client/quality_test.py
@@ -0,0 +1,24 @@
+import timeit
+
+
+class QualityTest:
+
+    SAMPLE_SIZE = 10000
+
+    def get_quality(self):
+        return (self._test_floating_point_speed() +
+                self._test_string_speed() +
+                self._test_lookup_speed())
+
+    def _test_floating_point_speed(self):
+        return timeit.timeit("[x * 3.1415 for x in range(100)]",
+                             number=QualityTest.SAMPLE_SIZE)
+
+    def _test_string_speed(self):
+        return timeit.timeit("'-'.join([str(x) for x in range(100)])",
+                             number=QualityTest.SAMPLE_SIZE)
+
+    def _test_lookup_speed(self):
+        return timeit.timeit("[a[x] for x in range(100)]",
+                             setup="a = {x:str(x) for x in range(100)}",
+                             number=QualityTest.SAMPLE_SIZE)
