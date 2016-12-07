4e72b319b86db71641018184a843b99779019b6c
iandioch <iandioch11@gmail.com>
2016-12-07 08:43:30 -0500

    Add apply_distributable to hello_world.py

--- examples/hello_world.py
+++ examples/hello_world.py
@@ -10,6 +10,22 @@ input_data = ["1", "2", "3", "4", "5"]
 def read_function(input_value):
     return int(input_value) * 2
 
+
+@Dipla.distributable
+def get_factors(n):
+    facts = [1]
+    for i in range(2, n//2 + 1):
+        if n % i == 0:
+            facts.append(i)
+    facts.append(n)
+    return facts
+
+
 # Convert the input_data list to a form that dipla can use for
 # distributing
 data_source = Dipla.read_data_source(read_function, input_data) 
+
+# Apply a distributable function to the stream of values from
+# the above source
+factor_results = Dipla.apply_distributable(get_factors,
+                                           data_source)
