ee83a223dafd00f5d404673b385a0b951d1537f0
byxor <brandnewbyxor@gmail.com>
2017-01-31 19:15:11 +0000

    Add StatisticsUpdater and StatisticsReader classes
    Add & test StatisticsReader class
    
    Address PEP8 warnings

---
+++ dipla/client/statistics.py
@@ -0,0 +1,48 @@
+class StatisticsUpdater:
+    """
+    A class to update and modify statistics.
+    All statistics modification should happen through this class exclusively.
+    """
+
+    def __init__(self, statistics):
+        """Creates a StatisticsUpdater, should ideally be called once."""
+        self.__statistics = statistics
+
+    def increment(self, statistic):
+        """Increments an integer statistic by 1."""
+        check_statistic_exists(statistic, self.__statistics)
+        self.__statistics[statistic] += 1
+
+    def overwrite(self, statistic, value):
+        """Overwrites a statistic's old value with a new one."""
+        check_statistic_exists(statistic, self.__statistics)
+        self.__statistics[statistic] = value
+
+
+class StatisticsReader:
+    """
+    A class to read statistics in an immutable way.
+    """
+
+    def __init__(self, statistics):
+        self.__statistics = statistics
+
+    def read(self, statistic):
+        """Fetch the value for a statistic."""
+        check_statistic_exists(statistic, self.__statistics)
+        return self.__statistics[statistic]
+
+    def read_all(self):
+        """Fetch an immutable dictionary of all statistics."""
+        return self.__statistics.copy()
+
+
+class StatisticsError(Exception):
+    pass
+
+
+def check_statistic_exists(statistic, statistics):
+    """Raise a StatisticsError if the statistic doesn't exist."""
+    if statistic not in statistics:
+        error_message = "Statistic {} does not exist".format(statistic)
+        raise StatisticsError(error_message)
