414d9e514bc255fc1e552e4ab1ae2003580ebc3c
iandioch <iandioch11@gmail.com>
2017-02-16 17:29:05 +0000

    Add React components for App and WorkerStatus

--- dashboard/static/app.js
+++ dashboard/static/app.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-/*(function(){
-    alert("hi");
-}());*/
+const updateIntervalInSeconds = 10;
 
-ReactDOM.render(
-    <h1>Hello domhan!</h1>,
-    document.getElementById('root')
-);
+function WorkerStatus(props) {
+    return <p>Idle workers: {props.idle} / {props.total}</p>;
+}
+
+function App(props) {
+    return (
+        <div>
+            <WorkerStatus />
+        </div>
+    );
+}
+
+function tick() {
+    console.log("HI there");
+    ReactDOM.render(
+        <App />,
+        document.getElementById('root')
+    );
+}
+
+tick();
+setInterval(tick, updateIntervalInSeconds*1000);
