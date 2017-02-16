e5b31314c9bcc363d531f31e46456e0cd5e406d5
iandioch <iandioch11@gmail.com>
2017-02-16 18:14:37 +0000

    Add functioning idle worker counter

--- dashboard/static/app.js
+++ dashboard/static/app.js
@@ -1,27 +1,69 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const updateIntervalInSeconds = 10;
+const updateIntervalInSeconds = 5;
 
 function WorkerStatus(props) {
     return <p>Idle workers: {props.idle} / {props.total}</p>;
 }
 
-function App(props) {
-    return (
-        <div>
-            <WorkerStatus />
-        </div>
-    );
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
+
+    componentDidMount() {
+        console.log("ComponentDidMount");
+        this.timerId = setInterval(
+            () => this.tick(),
+            updateIntervalInSeconds*1000
+        );
+        this.tick();
+    }
+
+    componentWillUnmount() {
+        console.log("ComponentWillUnmount");
+        clearInterval(this.timerId);
+    }
+
+    getJsonFromServer(callback) {
+        const xhr = new XMLHttpRequest();
+        xhr.open("get", "get_stats", true);
+        xhr.onload = () => {
+            const status = xhr.status;
+            if (status == 200) {
+                callback(xhr.response);
+            } else {
+                console.log("Got status " + status +
+                            " from REST server.");
+            }
+        };
+        xhr.send();
+    }
+
+    tick() {
+        this.getJsonFromServer((response) => {
+            console.log("json received: " + response);
+            if (response !== null) {
+                const data = JSON.parse(response);
+                this.setState(data);
+            }
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <WorkerStatus
+                    idle={this.state.num_idle_workers}
+                    total={this.state.num_total_workers} />
+            </div>
+        );
+    }
 }
 
-function tick() {
-    console.log("HI there");
-    ReactDOM.render(
-        <App />,
-        document.getElementById('root')
-    );
-}
-
-tick();
-setInterval(tick, updateIntervalInSeconds*1000);
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+);
