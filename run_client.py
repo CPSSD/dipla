6bd13084ad12e49f3b055224fb677bb677b2811f
Brandon Ibbotson <brandnewbyxor@gmail.com>
2017-03-09 13:04:29 +0000

    (Merge 6th) Partial PR: Migrate run_client and delete run_server (#212)
    * Migrate run_client and run_server
    
    * Delete run_server.py
    
    * Fix import error

--- run_client.py
+++ run_client.py
@@ -1,4 +1,5 @@
-from dipla.client.client import Client
+from dipla.client.quality_scorer import QualityScorer
+from dipla.client.client import Client, ClientEventListener
 from dipla.client.config_handler import ConfigHandler
 from dipla.client.client_services import RunInstructionsService
 from dipla.client.client_services import VerifyInputsService
@@ -6,13 +7,13 @@ from dipla.client.client_services import BinaryReceiverService
 from dipla.client.client_services import ServerErrorService
 from dipla.client.command_line_binary_runner import CommandLineBinaryRunner
 from dipla.shared.logutils import LogUtils
-import sys
-import argparse
+from dipla.shared.network.network_connection import ClientConnection
 from logging import FileHandler
+from argparse import ArgumentParser
 
 
-def main(argv):
-    parser = argparse.ArgumentParser(description="Start a Dipla client.")
+def main():
+    parser = ArgumentParser(description="Start a Dipla client.")
     parser.add_argument('-c', default='', dest='config_path')
     args = parser.parse_args()
 
@@ -22,13 +23,22 @@ def main(argv):
 
     init_logger(config.params['log_file'])
 
+    password = config.params['password']
+    server_ip = config.params['server_ip']
+    server_port = config.params['server_port']
+
+    services = create_services()
+    event_listener = ClientEventListener(services)
+    connection = ClientConnection(server_ip, server_port, event_listener)
+    quality_scorer = QualityScorer()
+
     client = Client(
-        'ws://{}:{}'.format(
-            config.params['server_ip'], config.params['server_port']),
-        password=config.params['password']
+        event_listener,
+        connection,
+        quality_scorer,
+        password
     )
-    services = create_services(client)
-    client.inject_services(services)
+
     client.start()
 
 
@@ -36,30 +46,27 @@ def init_logger(loc):
     LogUtils.init(handler=FileHandler(loc))
 
 
-def create_services(client):
+def create_services():
+    binary_paths = {}
+    binary_runner = CommandLineBinaryRunner()
+    binary_base_file_path = './'
     services = {
-        RunInstructionsService.get_label():
-            _create_run_instructions_service(client),
-        VerifyInputsService.get_label(): _create_verify_inputs_service(client),
-        BinaryReceiverService.get_label(): _create_binary_receiver(client),
-        ServerErrorService.get_label(): ServerErrorService(client)
+        RunInstructionsService.get_label(): RunInstructionsService(
+            binary_paths,
+            binary_runner
+        ),
+        VerifyInputsService.get_label(): VerifyInputsService(
+            binary_paths,
+            binary_runner
+        ),
+        BinaryReceiverService.get_label(): BinaryReceiverService(
+            binary_base_file_path,
+            binary_paths
+        ),
+        ServerErrorService.get_label(): ServerErrorService()
     }
     return services
 
 
-def _create_run_instructions_service(client):
-    binary_runner = CommandLineBinaryRunner()
-    return RunInstructionsService(client, binary_runner)
-
-
-def _create_verify_inputs_service(client):
-    binary_runner = CommandLineBinaryRunner()
-    return VerifyInputsService(client, binary_runner)
-
-
-def _create_binary_receiver(client):
-    return BinaryReceiverService(client, './binary')
-
-
 if __name__ == '__main__':
-    main(sys.argv)
+    main()
