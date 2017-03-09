30928b0122908e97e57366abc47f0f87294ff1c0
iandioch <iandioch11@gmail.com>
2017-03-09 14:26:19 -0500

    Add tests for other errors

--- tests/api/discovery_server_test.py
+++ tests/api/discovery_server_test.py
@@ -13,12 +13,39 @@ class InformDiscoveryServerTest(TestCase):
         def read(self):
             return self.response_data.encode()
 
-    @patch('dipla.api.urlopen')
+    @patch("dipla.api.urlopen")
     def test_conflict_error_handled(self, mock_urlopen):
         mock_urlopen.return_value = self.UrlResponseMock({
             "success": False,
             "error": "409: oh h*ck"
         })
         with self.assertRaises(DiscoveryConflict):
-            Dipla.inform_discovery_server('http://a.com', 'b', 'c', 'd')
+            Dipla.inform_discovery_server("http://a.com", "b", "c", "d")
 
+    @patch("dipla.api.urlopen")
+    def test_bad_request_error_handler(self, mock_urlopen):
+        mock_urlopen.return_value = self.UrlResponseMock({
+            "success": False,
+            "error": "400: you've done me a frighten"
+        })
+        with self.assertRaises(DiscoveryBadRequest):
+            Dipla.inform_discovery_server("http://a.com", "b", "c", "d")
+
+    @patch("dipla.api.urlopen")
+    def test_misc_error_handler(self, mock_urlopen):
+        mock_urlopen.return_value = self.UrlResponseMock({
+            "success": False,
+            "error": "418: bork bork the tea is ready"
+        })
+        with self.assertRaises(RuntimeError):
+            Dipla.inform_discovery_server("http://a.com", "b", "c", "d")
+
+    @patch("dipla.api.urlopen")
+    def test_runs_with_no_error(self, mock_urlopen):
+        mock_urlopen.return_value = self.UrlResponseMock({
+            "success": True
+        })
+        try:
+            Dipla.inform_discovery_server("http://a.com", "b", "c", "d")
+        except:
+            self.fail("Unhandled error while informing server")
