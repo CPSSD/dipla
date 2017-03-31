54ccdf0a21ed9be63adae08d97b962ae75d4c197
byxor <brandnewbyxor@gmail.com>
2017-03-31 09:53:14 +0100

    Add mapreduce example

--- examples/map_reduce.py
+++ examples/map_reduce.py
@@ -1,50 +1,46 @@
+import functools
 from os.path import abspath
 import sys
 sys.path.append(abspath('../dipla'))
 from dipla.api import Dipla
 
 
-COMPUTATIONAL_INTENSITY = 1000
-
-
-@Dipla.data_source
-def read(input_value):
-    return input_value
+def generate_urls():
+    base_url = '/home/brandon/.webpages/euler/problem={}'
+    number_of_urls = 586
+    for i in range(number_of_urls):
+        yield base_url.format(i + 1)
 
 
 @Dipla.distributable()
-def square(n):
-    return int(n)**2
-
-
-def generate_inputs():
-    inputs = []
-    for n in range(COMPUTATIONAL_INTENSITY):
-        inputs.append(str(n))
-        if len(inputs) == 5:
-            yield inputs
-            inputs = []
-    yield inputs
+def count_word_on_web_page(url):
+    try:
+        import bs4 as bs
+        import re
+        file = open(url, 'rt')
+        raw_page = file.read()
+        file.close()
+        soup = bs.BeautifulSoup(raw_page, 'html.parser')
+        pattern = re.compile('primes'.lower())
+        all_matches = pattern.findall(soup.text.lower())
+        answer = len(all_matches) if all_matches is not None else 0
+        return answer
+    except Exception as e:
+        print(e)
 
 
 def main():
-    # Dipla.start_dashboard()
 
-    data_source = Dipla.read_data_source(
-        read,
-        generate_inputs()
+    word_count_promise = Dipla.apply_distributable(
+        count_word_on_web_page,
+        generate_urls()
     )
 
-    squared_inputs_promise = Dipla.apply_distributable(
-        square,
-        data_source
-    )
+    word_counts = [int(s) for s in Dipla.get(word_count_promise)]
+    print(word_counts)
+    # total_word_count = sum(word_counts)
+    # print(total_word_count)
 
-    squared_inputs = [int(s) for s in Dipla.get(squared_inputs_promise)]
-    sum_of_squared_inputs = sum(squared_inputs)
-    print(sum_of_squared_inputs)
-    
 
 if __name__ == '__main__':
     main()
-
